29aeaebf72fca125aa1bfcd0987b39564cb2c770
DipanshuChawde <dipanshuchoude@gmail.com>
2025-04-30 19:59:51 +0530

    30Apr25

---
+++ tests/8.radioButtons.spec.js
@@ -0,0 +1,21 @@
+import { test, expect } from "@playwright/test"
+
+test('verify assertions in playwright', async ({ page }) => {
+    await page.goto('https://testautomationpractice.blogspot.com/')
+
+    await page.fill('#name', 'dipanshu')
+    //await page.locator('#name').fill('dipanshu)
+
+    await page.fill('#email', 'dipanshu@gmail.com')
+    await page.fill('#phone', '9922447802')
+    await page.fill('#textarea', 'hi')
+
+    //radio buttons
+    let male = await page.locator('#male')
+    await expect(male).not.toBeChecked()
+    await male.check()
+    await expect(male).toBeChecked()
+
+    await page.waitForTimeout(3000)
+
+})
