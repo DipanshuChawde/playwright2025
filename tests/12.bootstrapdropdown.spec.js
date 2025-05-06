f9de0bf0a067b84a4ec02a7209222526586738eb
DipanshuChawde <dipanshuchoude@gmail.com>
2025-05-06 20:34:02 +0530

    6May25

---
+++ tests/12.bootstrapdropdown.spec.js
@@ -0,0 +1,33 @@
+import { test, expect } from "@playwright/test"
+
+test('verify dropdown in playwright', async ({ page }) => {
+    await page.goto('https://www.jquery-az.com/boots/demo.php?ex=63.0_2')
+    await page.click('.btn-group')
+
+    //count options
+    let options = await page.locator('ul > li >a > label >input')
+    await expect(options).toHaveCount(11)
+
+    let options2 = await page.$$('ul > li >a > label >input')
+    await expect(options2.length).toBe(11)
+
+    //print options
+    let options3 = await page.$$('ul > li >a > label')
+    for (let opn of options3) {
+        console.log(await opn.textContent())
+    }
+
+    // find presence of option
+    let status = false
+    for (let opn of options3) {
+        let optionName = await opn.textContent()
+        if(optionName.includes(' MS SQL Server')){
+            status=true
+            break;
+        }
+    }
+    await expect(status).toBeTruthy()
+
+    await page.waitForTimeout(5000)
+
+})
