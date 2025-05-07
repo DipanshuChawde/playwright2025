1c0be49686c0add3e91887f3630d50b70ee83bca
DipanshuChawde <dipanshuchoude@gmail.com>
2025-05-07 19:59:59 +0530

    7May25

--- tests/12.bootstrapdropdown.spec.js
+++ tests/12.bootstrapdropdown.spec.js
@@ -13,21 +13,72 @@ test('verify dropdown in playwright', async ({ page }) => {
 
     //print options
     let options3 = await page.$$('ul > li >a > label')
-    for (let opn of options3) {
-        console.log(await opn.textContent())
-    }
+    // for (let opn of options3) {
+    //     console.log(await opn.textContent())
+    // }
 
     // find presence of option
     let status = false
     for (let opn of options3) {
         let optionName = await opn.textContent()
-        if(optionName.includes(' MS SQL Server')){
-            status=true
+        if (optionName.includes(' MS SQL Server')) {
+            status = true
             break;
         }
     }
     await expect(status).toBeTruthy()
 
+
+    //print options 
+    let option4 = await page.$$('ul > li > a > label.checkbox')
+    for (let opn of option4) {
+        console.log(await opn.textContent())
+    }
+
+    //select single options 
+    for (let opn of option4) {
+        let opnTxt = await opn.textContent()
+        if (opnTxt == " Python") {
+            opn.check()
+        }
+    }
+
+    //select multiple options 
+    let optionToSelect = [" Java", " MySQL", " Oracle"]
+    for (let opn of option4) {
+        let opnTxt = await opn.textContent()
+        if (optionToSelect.includes(opnTxt)) {
+            await opn.check()
+        }
+    }
+    //check unchecked options
+    // for (let opn of option4) {
+    //     await opn.check()
+    // }
+
+    //check unchecked options---type1
+    // for (let opn of option4) {
+    //     await opn.uncheck()
+    // }
+
+    //uncheck checked options ---type2
+    // for (let opn of option4) {
+    //     if(await opn.isChecked())
+    //     await opn.click()
+    // }
+
+    //check unchecked options
+    for (let opn of option4) {
+        if (!(await opn.isChecked()))
+            await opn.click()
+    }
+
+    // uncheck all
+    for (let opn of option4) {
+        if ((await opn.isChecked()))
+            await opn.click()
+    }
     await page.waitForTimeout(5000)
 
 })
+
