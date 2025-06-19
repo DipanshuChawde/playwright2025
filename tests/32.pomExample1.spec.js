3cfe8f660cf5204f857e325c77afc15cced5f212
DipanshuChawde <dipanshuchoude@gmail.com>
2025-06-19 20:43:23 +0530

    19June25

--- tests/32.pomExample1.spec.js
+++ tests/32.pomExample1.spec.js
@@ -2,13 +2,16 @@
 //It helps in maintaining, scaling, and reusing test automation code by separating the test logic from the page structure.
 import { expect ,test} from "@playwright/test";
 import { myClass } from "../pages/pageFor32file"
+import productList from "../payload/productsList.json"
 
 test('verify pom page structure in playwright',async({page})=>{
+    await page.setDefaultTimeout(60000)
     const hp= new myClass(page)
     await hp.visitUrl()
     await hp.login('dipanshu','dipanshu@123')
 
     await page.waitForTimeout(2000)
+    //await hp.addToCart('phone',productList.phone)
+    await hp.addToCart('laptop',productList.laptop)
   
- 
 })
